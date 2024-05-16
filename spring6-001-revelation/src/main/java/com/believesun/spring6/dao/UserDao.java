8c2321734ba63fcc65cd1bdf2b8f3d098e35bc3b
HHHH <HHHH@qq.com>
2024-05-16 09:53:37 +0800

    第一章

---
+++ spring6-001-revelation/src/main/java/com/believesun/spring6/dao/UserDao.java
@@ -0,0 +1,5 @@
+package com.believesun.spring6.dao;
+
+public interface UserDao {
+    void UserDelete();
+}
---
+++ spring6-001-revelation/src/main/java/com/believesun/spring6/service/impl/UserServiceImpl.java
@@ -0,0 +1,14 @@
+package com.believesun.spring6.service.impl;
+
+import com.believesun.spring6.dao.UserDao;
+import com.believesun.spring6.dao.impl.UserDaoImplByMysql;
+import com.believesun.spring6.service.UserService;
+
+public class UserServiceImpl implements UserService {
+    // 引入DIP原则
+    private UserDao userDao = new UserDaoImplByMysql();
+    @Override
+    public void UserServiceDelete() {
+        userDao.UserDelete();
+    }
+}
